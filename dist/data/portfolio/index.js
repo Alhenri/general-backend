0a5eae718e1c18caaa7655f33cc706a69993dcd6
Alexandre Henrique Soares <ahssf01@gmail.com>
2021-05-29 16:41:37 -0300

    remove dist

--- dist/data/portfolio/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _about_me = require('./about_me'); var _about_me2 = _interopRequireDefault(_about_me);
-var _projects = require('./projects'); var _projects2 = _interopRequireDefault(_projects);
-var _qualification = require('./qualification'); var _qualification2 = _interopRequireDefault(_qualification);
-
-exports. default = {
-  about_me: _about_me2.default,
-  projects: _projects2.default,
-  qualification: _qualification2.default,
-};
